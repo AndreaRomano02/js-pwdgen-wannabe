7c254a32cebc6517d7dd0f39b3936543ba4c62f9
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-25 14:32:02 +0200

    JS complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log('JS OK')
+
+//! Preliminari
+const myPassword = document.getElementById('my-psw');
+console.log(myPassword);
+
+//! Funzioni
+//? FisrtName
+const firstname = prompt('Come ti chiami?', 'Andrea');
+console.log(firstname);
+
+//? LastName
+const lastname = prompt('Qual\' è il tuo cognome?', 'Romano');
+console.log(lastname);
+
+//? Favourite Color
+const favouriteColor = prompt('Qual\' è il tuo colore preferito?', 'Rosso');
+console.log(favouriteColor);
+
+//* Create Password
+const psw = firstname + lastname + favouriteColor + '21';
+console.log(psw);
+
+//* Stampo la password
+myPassword.innerText = psw;
